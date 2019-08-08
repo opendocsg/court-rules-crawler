d6feacbbbc7aaf9ec8dd65ff86d30d4c318246ee
LoneRifle <LoneRifle@users.noreply.github.com>
2019-08-08 17:25:12 +0800

    Add construction of structured provision (broken)

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
 
+const TurndownService = require('turndown')
+const turndownPluginGfm = require('turndown-plugin-gfm')
+const turndown = new TurndownService({ headingStyle: 'atx' })
+turndown.use(turndownPluginGfm.gfm)
+
+turndown.keep(['sup', 'div'])
+
 const RULES_URL = 'https://sso.agc.gov.sg/SL/SCJA1969-R5'
 
 function makeIndexPage (rawIndexPage) {
@@ -29,20 +36,59 @@ ${getText(indexData, '.cDate')}
 `
 }
 
-function makeProvisionSection (provision) {
+function makeStructuredProvision(provision, $, indent = 0) {
+  const content = provision.contents().get()
+    .filter(e => $(e).prop('tagName') !== 'A')
+    .filter(e => $(e).prop('tagName') !== 'STRONG')
+    .map(e => {
+      let element = $(e)
+      if (!element.prop('tagName')) {
+        return element.text()
+      }
+      if (element.prop('tagName') === 'TABLE') {
+        element = element.children('tbody').children('tr').children('td')
+      }
+      const subContent = element.contents().get()
+        .map(c => {
+          let child = $(c)
+          if (!child.prop('tagName')) {
+            return child.text()
+          } else if (child.prop('tagName') === 'DIV' && child.hasClass('table-responsive')) {
+            return child.children('table').children('tbody').children('tr').children('td').html()
+          } else if (child.prop('tagName') === 'TABLE') {
+            child = child.children('tbody').children('tr').children('td')
+            return makeStructuredProvision(child, $, indent + 1)
+          } else if (child.prop('tagName') === 'EM'){
+            return `_${child.text()}_`
+          } else {
+            return child.get(0).outerHTML
+          }
+        })
+
+      // TODO: Join content in smarter fashion
+      return subContent.join('\n\n')
+    })
+
+  const prefix = indent === 0 && provision.children().first().prop('tagName') === 'STRONG'
+    ? `**${provision.children('strong').html()}**` : ''
+  return prefix + content.map(t => '>'.repeat(indent) + t).join('\n\n')
+}
+
+function makeProvision (provision, $) {
   if (provision.hasClass('prov1Hdr')) {
     return `## ${provision.text()}`
   } else if (!provision.children('a').length) {
-    return provision.html()
+    // Simple one-clause provision
+    return turndown.turndown(provision.html())
   } else {
-    return 'Structured Text'
+    return makeStructuredProvision(provision, $)
   }
 }
 
 function makeOrderPage (order, $) {
   const orderTitle = order.find('.orderHdr').map(function () { return $(this).text() }).get().join(' - ')
   const provisions = order.find('div[class^=prov1] > table > tbody > tr > td')
-    .map(function () { return makeProvisionSection($(this)) })
+    .map(function () { return makeProvision($(this), $) })
   return '' +
 `# ${orderTitle}
 
